ce5e287b4a919eae4467e6a8107436f96de0938c
sachin supunthaka <sachinsupunthaka96@gmail.com>
2024-10-08 15:34:40 +0530

    chor: crud with monorepo repository attern

---
+++ apps/reservations/src/reservations.repository.ts
@@ -0,0 +1,19 @@
+import { AbstractRepository } from '@app/common';
+import { Injectable, Logger } from '@nestjs/common';
+
+import { ReservationDocument } from './models/reservation.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+@Injectable()
+export class ReservationRepository extends AbstractRepository<ReservationDocument> {
+  protected logger: Logger;
+  protected readonly loggr = new Logger(ReservationRepository.name);
+
+  constructor(
+    @InjectModel(ReservationDocument.name)
+    reservationModel: Model<ReservationDocument>,
+  ) {
+    super(reservationModel);
+  }
+}
